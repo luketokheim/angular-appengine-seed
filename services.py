6652c49680c1ad8bfdfb84896069a1cac960b628
Luke Tokheim <luketokheim@gmail.com>
2013-12-12 10:52:53 -0800

    AppEngine: Bumped version of endpoints_proto_datastore to match changes in Cloud Endpoints. Removed appstats logging.

--- services.py
+++ services.py
@@ -1,8 +1,6 @@
 import fix_path
 import endpoints
-from google.appengine.ext.appstats import recording
 
 from service import seed
 
 app = endpoints.api_server([seed.Api], restricted=False)
-app = recording.appstats_wsgi_middleware(app)
